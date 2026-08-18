124ab4477f973dd8ffe35751123e84ab7f26ba9b
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-18 18:24:59 -0300

    Improved kernel implementation

--- lib/euclidean_distance.ex
+++ lib/euclidean_distance.ex
@@ -16,9 +16,9 @@ PolyHok.defmodule EuclideanDistance do
   # x - conjunto X contendo 'len_x' vetores
   defk euclidean_distance_kernel(q, x, len_x, num_dims, arr_results) do
     g_idx = get_global_id(0)
-    x_idx = g_idx * num_dims
 
-    if x_idx < len_x do
+    if g_idx < len_x do
+      x_idx = g_idx * num_dims
       arr_results[g_idx] = calculate_euclidean(q, x + x_idx, num_dims)
     end
   end
@@ -28,7 +28,7 @@ PolyHok.defmodule EuclideanDistance do
     x_gnx = x |> PolyHok.new_gnx()
     arr_results_gnx = PolyHok.new_gnx({len_x}, :f32)
 
-    threads_per_block = 128
+    threads_per_block = 512
     num_blocks = div(len_x + threads_per_block - 1, threads_per_block)
 
     PolyHok.spawn(
